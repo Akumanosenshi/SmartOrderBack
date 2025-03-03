98725063061a5ad4fdeb3040478724e1917db372
Akumanosenshi <alison.perard.grans@gmail.com>
2025-03-03 14:30:14 +0100

    Spring initializer

---
+++ src/test/java/smartorder/alison/SmartorderApplicationTests.java
@@ -0,0 +1,13 @@
+package smartorder.alison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
