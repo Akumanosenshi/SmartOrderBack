4a97efba70c8e027c8c7f0720f46b07534765e96
Perard Alison <alison.perard.grans@gmail.com>
2025-07-30 14:59:40 +0200

    clean

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:5000/alert"
-payload = {"alerts": ["Service API", "Base de données"]}
-
-resp = requests.post(url, json=payload)
-print("Statut HTTP :", resp.status_code)
-print("Réponse JSON :", resp.json())
